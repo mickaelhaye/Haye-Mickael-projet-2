71820da79249c2ab616ed4de1a2d160403c8cc2f
Mickael Haye <8d4tffp98jq7-student@anonymous-codio.com>
2023-03-09 12:58:57 +0000

    writer implementation

--- com/hemebiotech/analytics/ISymptomWriter.java
+++ com/hemebiotech/analytics/ISymptomWriter.java
@@ -8,5 +8,5 @@ import java.util.Map;
  *
  */
 public interface ISymptomWriter {
-// WRITE YOUR CODE HERE
+public void writeSymptoms(Map<String, Integer> symptoms);
 }
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,8 +1,24 @@
 package com.hemebiotech.analytics;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class WriteSymptomDataToFile implements ISymptomWriter {
+ @Override
+  public void writeSymptoms(Map<String, Integer> symptoms) {
+    try {
+      FileWriter fileWriter = new FileWriter("result.out");
+      BufferedWriter writer = new BufferedWriter(fileWriter);
+      for (Map.Entry<String, Integer> entry:symptoms.entrySet()) {      
+        writer.write(entry.getKey() + " : " + entry.getValue());
+        writer.newLine();
+      }
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
