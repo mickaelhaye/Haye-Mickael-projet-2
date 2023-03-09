391c855b2b49683a136e7c37420eef295cf114c5
Mickael Haye <8d4tffp98jq7-student@anonymous-codio.com>
2023-03-09 13:04:34 +0000

    refactor counter

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,11 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
@@ -39,5 +44,40 @@ public class AnalyticsCounter {
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+	}
+	private ISymptomReader reader;
+  private ISymptomWriter writer;
+
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+    this.reader = reader;
+    this.writer = writer;
+  }
+	public List<String> getSymptoms() {
+    return this.reader.getSymptoms();
+  }
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+    Map<String, Integer> map = new HashMap<String, Integer>();
+    for (int i = 0; i < symptoms.size(); i++) {
+      if (map.containsKey(symptoms.get(i))) {
+        String keyMap = symptoms.get(i);
+        int valMap = map.get(keyMap);
+        valMap++;
+        map.put(keyMap, valMap);
+      } else {
+        map.put(symptoms.get(i), 1);
+      }
+    }
+    return map;
+  }
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+    Map<String, Integer> mapSort = new TreeMap<String, Integer>(symptoms);
+    return mapSort;
+  }
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+    try {
+      this.writer.writeSymptoms(symptoms);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 	}
 }
